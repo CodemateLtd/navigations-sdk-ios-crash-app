a2d1f90c72c23b9194c32031c3b96c37f4a6ae03
Joonas Kerttula <joonas.kerttula@codemate.com>
2025-09-08 09:11:39 +0300

    Initial Commit

---
+++ crashtest/crashtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  crashtestApp.swift
+//  crashtest
+//
+//  Created by Joonas Kerttula on 8.9.2025.
+//
+
+import SwiftUI
+
+@main
+struct crashtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
